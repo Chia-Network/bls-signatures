58f4fe826a1f6436b4b679643636f9ebe0fa9f0d
Alexander Block <ablock84@gmail.com>
2018-09-19 20:56:44 +0200

    Always include <gmp.h> before including relic headers
    The relic headers internally include <gmp.h> as well. At the same time,
    inclusion of relic headers is wrapped into a namespace, which makes the
    gmp headers be part of that namespace as well. This leads to conflicting
    definitions of "std::ostream& operator<< (std::ostream &, mpz_srcptr)" and
    friends on Debian stretch (if gmp from /usr is used).

--- src/extendedprivatekey.hpp
+++ src/extendedprivatekey.hpp
@@ -15,6 +15,12 @@
 #ifndef SRC_EXTENDEDPRIVATEKEY_HPP_
 #define SRC_EXTENDEDPRIVATEKEY_HPP_
 
+#include "relic_conf.h"
+
+#if defined GMP && ARITH == GMP
+#include <gmp.h>
+#endif
+
 #include "blsprivatekey.hpp"
 #include "blspublickey.hpp"
 #include "chaincode.hpp"
