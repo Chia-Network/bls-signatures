3fc79d6a9004cb8c5089c9892ed9f67b3273afd4
Anton Suprunchuk <antouhou@gmail.com>
2019-02-18 14:20:21 +0700

    Modify main cmake file to include specific emscripten instructions

--- js_build.sh
+++ js_build.sh
@@ -5,5 +5,5 @@ git submodule update --init --recursive
 mkdir js_build
 cd js_build
 
-cmake ../ -DARCH= -DWSIZE=32 -DCMAKE_TOOLCHAIN_FILE=/home/anton/Programs/emsdk/emscripten/1.38.25/cmake/Modules/Platform/Emscripten.cmake
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=/home/anton/Programs/emsdk/emscripten/1.38.25/cmake/Modules/Platform/Emscripten.cmake
 cmake --build . --
