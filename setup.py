fa2ec3dcbb4296c438d40cd891a16e7fa994e2c9
Gene Hoffman <hoffmang@hoffmang.com>
2020-07-10 19:37:35 -0700

    find libsodium on windows

--- setup.py
+++ setup.py
@@ -98,9 +98,9 @@ ext_modules = [
             get_pybind_include(user=True),
             'relic_ietf_64/include',
             'mpir_gc_x64',
-            'libsodium1.0.18/include',
+            'libsodium1.0.18/runtimes/include',
         ],
-        library_dirs=['relic_ietf_64', 'mpir_gc_x64', 'libsodium1.0.18/lib'],
+        library_dirs=['relic_ietf_64', 'mpir_gc_x64', 'libsodium1.0.18/runtimes'],
         libraries=['relic_s', 'Advapi32', 'mpir', 'libsodium'],
         language='c++'
     ),
