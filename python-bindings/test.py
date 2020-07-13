3570dc2d9a7e8cbfc1a714fbf2c3f2966df4dd53
Alex Wice <alex@chia.net>
2020-07-13 18:32:18 +1000

    pyecc tests

--- python-bindings/test.py
+++ python-bindings/test.py
@@ -179,338 +179,84 @@ def test_vectors_invalid():
             pass
 
 def test_vectors_valid():
-
-"""
-
-def test2():
-    seed = bytes([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    seed2 = bytes([1, 20, 102, 229, 1, 157])
-
-    sk = PrivateKey.from_seed(seed)
-    sk_cp = PrivateKey.from_seed(seed)
-    sk2 = PrivateKey.from_seed(seed2)
-    pk = sk.get_public_key()
-    pk2 = sk2.get_public_key()
-    assert(sk == sk_cp)
-    assert(sk != sk2)
-    assert(pk.get_fingerprint() == 0xddad59bb)
-
-    sk2_ser = sk2.serialize()
-    pk2_ser = pk2.serialize()
-    pk2_copy = PublicKey.from_bytes(pk2_ser)
-    assert(pk2 == pk2_copy)
-    assert(pk != pk2)
-    assert(len(pk2_ser) == 48)
-    assert(len(sk2_ser) == 32)
-
-    message = bytes("this is the message", "utf-8")
-    sig = sk.sign(message)
-    sig_ser = sig.serialize()
-    sig_cp = Signature.from_bytes(sig_ser)
-    a1 = AggregationInfo.from_msg(pk, message)
-    sig_cp.set_aggregation_info(a1)
-    a2 = sig_cp.get_aggregation_info()
-    assert(a1 == a2)
-    sig2 = sk2.sign(message)
-
-    assert(len(sig_ser) == 96)
-    assert(sig != sig2)
-    assert(sig == sig_cp)
-
-    sig_agg = Signature.aggregate([sig, sig2])
-
-    result = sig_cp.verify()
-    result2 = sig2.verify()
-    result3 = sig_agg.verify()
-    assert(result)
-    assert(result2)
-    assert(result3)
-    sk2 = sk
-
-def test_threshold_instance(T, N):
-    commitments = []
-    # fragments[i][j] = fragment held by player i,
-    #                   received from player j
-    fragments = [[None] * N for _ in range(N)]
-    secrets = []
-
-    # Step 1 : Threshold.create
-    for player in range(N):
-        secret_key, commi, frags = Threshold.create(T, N)
-        for target, frag in enumerate(frags):
-            fragments[target][player] = frag
-        commitments.append(commi)
-        secrets.append(secret_key)
-
-    # Step 2 : Threshold.verify_secret_fragment
-    for player_source in range(1, N+1):
-        for player_target in range(1, N+1):
-            assert Threshold.verify_secret_fragment(
-                player_target, fragments[player_target - 1][player_source - 1],
-                commitments[player_source - 1], T)
-
-    # Step 3 : master_pubkey = PublicKey.aggregate_insecure(...)
-    #          secret_share = PrivateKey.aggregate_insecure(...)
-    master_pubkey = PublicKey.aggregate_insecure([commitments[i][0] for i in range(N)])
-    secret_shares = [PrivateKey.aggregate_insecure(fragment_row) for fragment_row in fragments]
-    master_privkey = PrivateKey.aggregate_insecure(secrets)
-
-    msg = ("Test").encode("utf-8")
-    signature_actual = master_privkey.sign_insecure(msg)
-
-    # Step 4 : sig_share = Threshold.sign_with_coefficient(...)
-    # Check every combination of T players
-    for X in combinations(range(1, N+1), T):
-        # X: a list of T indices like [1, 2, 5]
-
-        # Check signatures
-        signature_shares = [Threshold.sign_with_coefficient(secret_shares[x-1], msg, x, X)
-                            for x in X]
-        signature_cand = InsecureSignature.aggregate(signature_shares)
-        assert signature_cand == signature_actual
-
-    # Check that the signature actually verifies the message
-    assert signature_actual.verify([Util.hash256(msg)], [master_pubkey])
-
-    # Step 4b : Alternatively, we can add the lagrange coefficients
-    # to 'unit' signatures.
-    for X in combinations(range(1, N+1), T):
-        # X: a list of T indices like [1, 2, 5]
-
-        # Check signatures
-        signature_shares = [secret_shares[x-1].sign_insecure(msg) for x in X]
-        signature_cand = Threshold.aggregate_unit_sigs(signature_shares, X)
-        assert signature_cand == signature_actual
-
-
-def test_threshold():
-    test_threshold_instance(1, 1)
-    test_threshold_instance(1, 2)
-    test_threshold_instance(2, 2)
-    for T in range(1, 6):
-        test_threshold_instance(T, 5)
-
-
-def test_vectors():
-    sk1 = PrivateKey.from_seed(bytes([1, 2, 3, 4, 5]))
-    pk1 = sk1.get_public_key()
-    sig1 = sk1.sign(bytes([7, 8, 9]))
-
-    sk2 = PrivateKey.from_seed(bytes([1, 2, 3, 4, 5, 6]))
-    pk2 = sk2.get_public_key()
-    sig2 = sk2.sign(bytes([7, 8, 9]))
-    assert(sk1.serialize() == bytes.fromhex("022fb42c08c12de3a6af053880199806532e79515f94e83461612101f9412f9e"))
-    assert(pk1.get_fingerprint() == 0x26d53247)
-    assert(pk2.get_fingerprint() == 0x289bb56e)
-    assert(sig1.serialize() == bytes.fromhex("93eb2e1cb5efcfb31f2c08b235e8203a67265bc6a13d9f0ab77727293b74a357ff0459ac210dc851fcb8a60cb7d393a419915cfcf83908ddbeac32039aaa3e8fea82efcb3ba4f740f20c76df5e97109b57370ae32d9b70d256a98942e5806065"))
-    assert(sig2.serialize() == bytes.fromhex("975b5daa64b915be19b5ac6d47bc1c2fc832d2fb8ca3e95c4805d8216f95cf2bdbb36cc23645f52040e381550727db420b523b57d494959e0e8c0c6060c46cf173872897f14d43b2ac2aec52fc7b46c02c5699ff7a10beba24d3ced4e89c821e"))
-
-    agg_sig = Signature.aggregate([sig1, sig2])
-    agg_pk = PublicKey.aggregate([pk1, pk2])
-    agg_sk = PrivateKey.aggregate([sk1, sk2], [pk1, pk2])
-    assert(agg_sig.serialize() == bytes.fromhex("0a638495c1403b25be391ed44c0ab013390026b5892c796a85ede46310ff7d0e0671f86ebe0e8f56bee80f28eb6d999c0a418c5fc52debac8fc338784cd32b76338d629dc2b4045a5833a357809795ef55ee3e9bee532edfc1d9c443bf5bc658"))
-    assert(agg_sk.sign(bytes([7, 8, 9])).serialize() == agg_sig.serialize())
-
-
-    assert(sig1.verify())
-    assert(agg_sig.verify())
-
-    agg_sig.set_aggregation_info(AggregationInfo.from_msg(agg_pk, bytes([7, 8, 9])))
-    assert(agg_sig.verify())
-
-    sig1.set_aggregation_info(sig2.get_aggregation_info())
-    assert(not sig1.verify())
-
-    sig3 = sk1.sign(bytes([1, 2, 3]))
-    sig4 = sk1.sign(bytes([1, 2, 3, 4]))
-    sig5 = sk2.sign(bytes([1, 2]))
-
-
-    agg_sig2 = Signature.aggregate([sig3, sig4, sig5])
-    assert(agg_sig2.verify())
-    assert(agg_sig2.serialize() == bytes.fromhex("8b11daf73cd05f2fe27809b74a7b4c65b1bb79cc1066bdf839d96b97e073c1a635d2ec048e0801b4a208118fdbbb63a516bab8755cc8d850862eeaa099540cd83621ff9db97b4ada857ef54c50715486217bd2ecb4517e05ab49380c041e159b"))
-
-
-def test_vectors2():
-    m1 = bytes([1, 2, 3, 40])
-    m2 = bytes([5, 6, 70, 201])
-    m3 = bytes([9, 10, 11, 12, 13])
-    m4 = bytes([15, 63, 244, 92, 0, 1])
-
-    sk1 = PrivateKey.from_seed(bytes([1, 2, 3, 4, 5]))
-    sk2 = PrivateKey.from_seed(bytes([1, 2, 3, 4, 5, 6]))
-
-    sig1 = sk1.sign(m1)
-    sig2 = sk2.sign(m2)
-    sig3 = sk2.sign(m1)
-    sig4 = sk1.sign(m3)
-    sig5 = sk1.sign(m1)
-    sig6 = sk1.sign(m4)
-
-    sig_L = Signature.aggregate([sig1, sig2])
-    sig_R = Signature.aggregate([sig3, sig4, sig5])
-    assert(sig_L.verify())
-    assert(sig_R.verify())
-
-    sig_final = Signature.aggregate([sig_L, sig_R, sig6])
-    assert(sig_final.serialize() == bytes.fromhex("07969958fbf82e65bd13ba0749990764cac81cf10d923af9fdd2723f1e3910c3fdb874a67f9d511bb7e4920f8c01232b12e2fb5e64a7c2d177a475dab5c3729ca1f580301ccdef809c57a8846890265d195b694fa414a2a3aa55c32837fddd80"))
-    assert(sig_final.verify())
-    quotient = sig_final.divide_by([sig2, sig5, sig6])
-    assert(quotient.verify())
-    assert(sig_final.verify())
-    assert(quotient.serialize() == bytes.fromhex("8ebc8a73a2291e689ce51769ff87e517be6089fd0627b2ce3cd2f0ee1ce134b39c4da40928954175014e9bbe623d845d0bdba8bfd2a85af9507ddf145579480132b676f027381314d983a63842fcc7bf5c8c088461e3ebb04dcf86b431d6238f"))
-    assert(quotient.divide_by([]) == quotient)
-    try:
-        quotient.divide_by([sig6])
-        assert(False)  # Should fail due to not subset
-    except:
-        pass
-    sig_final.divide_by([sig1]) # Should not throw
-    try:
-        sig_final.divide_by([sig_L]) # Should throw due to not unique
-        assert(False)  # Should fail due to not unique
-    except:
-        pass
-
-    # Divide by aggregate
-    sig7 = sk2.sign(m3)
-    sig8 = sk2.sign(m4)
-    sig_R2 = Signature.aggregate([sig7, sig8])
-    sig_final2 = Signature.aggregate([sig_final, sig_R2])
-    quotient2 = sig_final2.divide_by([sig_R2])
-    assert(quotient2.verify())
-    assert(quotient2.serialize() == bytes.fromhex("06af6930bd06838f2e4b00b62911fb290245cce503ccf5bfc2901459897731dd08fc4c56dbde75a11677ccfbfa61ab8b14735fddc66a02b7aeebb54ab9a41488f89f641d83d4515c4dd20dfcf28cbbccb1472c327f0780be3a90c005c58a47d3"))
-
-
-def test_vectors3():
-    seed = bytes([1, 50, 6, 244, 24, 199, 1, 25])
-    esk =  ExtendedPrivateKey.from_seed(seed)
-    assert(esk.get_public_key().get_fingerprint() == 0xa4700b27)
-    assert(esk.get_chain_code().serialize().hex() == "d8b12555b4cc5578951e4a7c80031e22019cc0dce168b3ed88115311b8feb1e3")
-    esk77 = esk.private_child(77 + 2**31)
-    assert(esk77.get_chain_code().serialize().hex() == "f2c8e4269bb3e54f8179a5c6976d92ca14c3260dd729981e9d15f53049fd698b")
-    assert(esk77.get_public_key().get_fingerprint() == 0xa8063dcf)
-
-    assert(esk.private_child(3)
-              .private_child(17)
-              .get_public_key()
-              .get_fingerprint() == 0xff26a31f)
-
-    assert(esk.get_extended_public_key()
-              .public_child(3)
-              .public_child(17)
-              .get_public_key()
-              .get_fingerprint() == 0xff26a31f)
-
-def test_vectors4():
-    sk1 = PrivateKey.from_seed(bytes([1, 2, 3, 4, 5]))
-    sk2 = PrivateKey.from_seed(bytes([1, 2, 3, 4, 5, 6]))
-
-    pk1 = sk1.get_public_key()
-    pk2 = sk2.get_public_key()
-
-    m1 = bytes([7, 8, 9])
-    m2 = bytes([10, 11, 12])
-
-    sig9 = sk1.sign_prepend(m1)
-    sig10 = sk2.sign_prepend(m2)
-
-    assert(sig9.serialize() == bytes.fromhex("d2135ad358405d9f2d4e68dc253d64b6049a821797817cffa5aa804086a8fb7b135175bb7183750e3aa19513db1552180f0b0ffd513c322f1c0c30a0a9c179f6e275e0109d4db7fa3e09694190947b17d890f3d58fe0b1866ec4d4f5a59b16ed"))
-    assert(sig10.serialize() == bytes.fromhex("cc58c982f9ee5817d4fbf22d529cfc6792b0fdcf2d2a8001686755868e10eb32b40e464e7fbfe30175a962f1972026f2087f0495ba6e293ac3cf271762cd6979b9413adc0ba7df153cf1f3faab6b893404c2e6d63351e48cd54e06e449965f08"))
-
-    agg_sig = PrependSignature.aggregate([sig9, sig9, sig10])
-    message_hashes =[Util.hash256(m1), Util.hash256(m1), Util.hash256(m2)]
-    pks = [pk1, pk1, pk2]
-    assert(agg_sig.serialize() == bytes.fromhex("c37077684e735e62e3f1fd17772a236b4115d4b581387733d3b97cab08b90918c7e91c23380c93e54be345544026f93505d41e6000392b82ab3c8af1b2e3954b0ef3f62c52fc89f99e646ff546881120396c449856428e672178e5e0e14ec894"))
-    assert(agg_sig.verify(message_hashes, pks))
-
-def no_throw_bad_sig():
-    private_key = ExtendedPrivateKey.from_seed(b"foo").get_private_key()
-
-    message_hash = bytes([9] * 32)
-
-    sig = private_key.sign_prepend_prehashed(message_hash).serialize()
-    sig = sig[:-1] + bytes([0])
-
-    public_key = private_key.get_public_key()
-
-    try:
-        bad_signature = PrependSignature.from_bytes(sig)
-    except ValueError:
-        return
-    assert(False)
-
-def throw_wrong_type():
-    private_key = ExtendedPrivateKey.from_seed(b"foo").get_private_key()
-
-    message_hash = bytes([10] * 32)
-
-    sig_prepend = private_key.sign_prepend_prehashed(message_hash).serialize()
-    sig_secure = private_key.sign_prehashed(message_hash).serialize()
-
-    try:
-        Signature.from_bytes(sig_prepend)
-    except ValueError:
-        try:
-            PrependSignature.from_bytes(sig_secure)
-        except ValueError:
-            return
-        assert False
-    assert False
-
-
-def additional_python_methods():
-    private_key = PrivateKey.from_seed(b'123')
-    s1 = private_key.sign(b'message')
-    s2 = private_key.sign_prepend(b'message')
-    assert s1.get_insecure_sig().verify([Util.hash256(b'message')], [private_key.get_public_key()])
-    assert s2.get_insecure_sig().verify([Util.hash256(private_key.get_public_key().serialize() +
-                                         Util.hash256(b'message'))], [private_key.get_public_key()])
-    s1_b = Signature.from_insecure_sig(s1.get_insecure_sig())
-    s2_b = PrependSignature.from_insecure_sig(s2.get_insecure_sig())
-    assert s1 == s1_b and s2 == s2_b
-
-    s3 = private_key.sign_insecure_prehashed(Util.hash256(b'456'))
-    assert s3.verify([Util.hash256(b'456')], [private_key.get_public_key()])
-
-    esk =  ExtendedPrivateKey.from_seed(b'789')
-    epk =  esk.get_public_key()
-    s3 = private_key.sign(b'message3')
-    s4 = private_key.sign_insecure(b'message3')
-
-    assert bytes(private_key) == private_key.serialize()
-    assert deepcopy(private_key) == private_key
-    assert deepcopy(s1) == s1
-    assert deepcopy(s2) == s2
-    assert deepcopy(s3) == s3
-    assert deepcopy(s4) == s4
-    assert deepcopy(private_key.get_public_key()) == private_key.get_public_key()
-    assert deepcopy(esk) == esk
-    assert deepcopy(epk) == epk
-    assert deepcopy(esk.get_chain_code()) == esk.get_chain_code()
-
-
-"""
+    # The following code was used to generate these vectors
+    """
+    from py_ecc.bls import (
+        G2Basic,
+        G2MessageAugmentation as G2MA,
+        G2ProofOfPossession as G2Pop,
+    )
+
+    secret1 = bytes([1] * 32)
+    secret2 = bytes([x * 314159 % 256 for x in range(32)])
+    sk1 = int.from_bytes(secret1, 'big')
+    sk2 = int.from_bytes(secret2, 'big')
+    msg = bytes([3, 1, 4, 1, 5, 9])
+    pk1 = G2Basic.SkToPk(sk1)
+    pk2 = G2Basic.SkToPk(sk2)
+
+    for Scheme in (G2Basic, G2MA, G2Pop):
+        sig1 = Scheme.Sign(sk1, msg)
+        sig2 = Scheme.Sign(sk2, msg)
+        sig_agg = Scheme.Aggregate([sig1, sig2])
+        print(sig1)
+        print(sig2)
+        print(sig_agg)
+    """
+
+    ref_sig1Basic = b'\x96\xba4\xfa\xc3<\x7f\x12\x9d`*\x0b\xc8\xa3\xd4?\x9a\xbc\x01N\xce\xaa\xb75\x91F\xb4\xb1P\xe5{\x80\x86Es\x8f5g\x1e\x9e\x10\xe0\xd8b\xa3\x0c\xabp\x07N\xb5\x83\x1d\x13\xe6\xa5\xb1b\xd0\x1e\xeb\xe6\x87\xd0\x16J\xdb\xd0\xa8d7\n|"*\'h\xd7pM\xa2T\xf1\xbf\x18#f[\xc26\x1f\x9d\xd8\xc0\x0e\x99'
+    ref_sig2Basic = b'\xa4\x02y\t2\x13\x0fvj\xf1\x1b\xa7\x16Sf\x83\xd8\xc4\xcf\xa5\x19G\xe4\xf9\x08\x1f\xed\xd6\x92\xd6\xdc\x0c\xac[\x90K\xee^\xa6\xe2Ui\xe3m{\xe4\xcaY\x06\x9a\x96\xe3K\x7fp\x07X\xb7\x16\xf9IJ\xaaY\xa9nt\xd1J;U*\x9ak\xc1)\xe7\x17\x19[\x9d`\x06\xfdm\\\xefGh\xc0"\xe0\xf71j\xbf'
+    ref_sigABasic = b'\x98|\xfd;\xcdb(\x02\x87\x02t\x83\xf2\x9cU$^\xd81\xf5\x1d\xd6\xbd\x99\x9ao\xf1\xa1\xf1\xf1\xf0\xb6Gw\x8b\x01g5\x9cqPUX\xa7n\x15\x8ef\x18\x1e\xe5\x12Y\x05\xa6B$k\x01\xe7\xfa^\xe5=h\xa4\xfe\x9b\xfb)\xa8\xe2f\x01\xf0\xb9\xadW}\xdd\x18\x87js1|!n\xa6\x1fC\x04\x14\xecQ\xc5'
+    ref_sig1Aug = b'\x81\x80\xf0,\xcbr\xe9"\xb1R\xfc\xed\xbe\x0e\x1d\x19R\x105Opp6X\xe8\xe0\x8c\xbe\xbf\x11\xd4\x97\x0e\xabj\xc3\xcc\xf7\x15\xf3\xfb\x87m\xf9\xa9yz\xbd\x0c\x1a\xf6\x1a\xae\xad\xc9,,\xfe\\\nV\xc1F\xcc\x8c?qQ\xa0s\xcf_\x16\xdf8$g$\xc4\xae\xd7?\xf3\x0e\xf5\xda\xa6\xaa\xca\xed\x1a&\xec\xaa3k'
+    ref_sig2Aug = b'\x99\x11\x1e\xea\xfbA-\xa6\x1eL7\xd3\xe8\x06\xc6\xfdj\xc9\xf3\x87\x0eT\xda\x92"\xbaNIH"\xc5\xb7eg1\xfazdY4\xd0KU\x9e\x92a\xb8b\x01\xbb\xeeW\x05RP\xa4Y\xa2\xda\x10\xe5\x1f\x9c\x1aiA)\x7f\xfc]\x97\nUr6\xd0\xbd\xeb|\xf8\xff\x18\x80\x0b\x08c8q\xa0\xf0\xa7\xeaB\xf4t\x80'
+    ref_sigAAug = b'\x8c]\x03\xf9\xda\xe7~\x19\xa5\x94Z\x06\xa2\x14\x83n\xdb\x8e\x03\xb8QR]\x84\xb9\xded@\xe6\x8f\xc0\xcas\x03\xee\xed9\r\x86<\x9bU\xa8\xcfmY\x14\n\x01\xb5\x88G\x88\x1e\xb5\xafgsMD\xb2UVF\xc6al9\xab\x88\xd2S)\x9a\xcc\x1e\xb1\xb1\x9d\xdb\x9b\xfc\xbev\xe2\x8a\xdd\xf6q\xd1\x16\xc0R\xbb\x18G'
+    ref_sig1Pop = b'\x95P\xfbN\x7f~\x8c\xc4\xa9\x0b\xe8V\n\xb5\xa7\x98\xb0\xb20\x00\xb6\xa5J!\x17R\x02\x10\xf9\x86\xf3\xf2\x81\xb3v\xf2Y\xc0\xb7\x80b\xd1\xeb1\x92\xb3\xd9\xbb\x04\x9fY\xec\xc1\xb0:pI\xebf^\r\xf3d\x94\xaeL\xb5\xf1\x13l\xca\xee\xfc\x99X\xcb0\xc33==C\xf0qH\xc3\x86)\x9a{\x1b\xfc\r\xc5\xcf|'
+    ref_sig2Pop = b"\xa6\x906\xbc\x11\xae^\xfc\xbfa\x80\xaf\xe3\x9a\xdd\xde~'s\x1e\xc4\x02W\xbf\xdc<7\xf1{\x8d\xf6\x83\x06\xa3N\xbd\x10\xe9\xe3*5%7P\xdf\\\x87\xc2\x14/\x82\x07\xe8\xd5eG\x12\xb4\xe5T\xf5\x85\xfbhF\xff8\x04\xe4)\xa9\xf8\xa1\xb4\xc5ku\xd0\x86\x9e\xd6u\x80\xd7\x89\x87\x0b\xab\xe2\xc7\xc8\xa9\xd5\x1e{*"
+    ref_sigAPop = b"\xa4\xeat+\xcd\xc1U>\x9c\xa4\xe5`\xbe~^ln\xfajd\xdd\xdf\x9c\xa3\xbb(T#=\x85\xa6\xaa\xc1\xb7n\xc7\xd1\x03\xdbN3\x14\x8b\x82\xaf\x99#\xdb\x05\x93Jn\xce\x9aq\x01\xcd\x8a\x9dG\xce'\x97\x80V\xb0\xf5\x90\x00!\x81\x8cEi\x8a\xfd\xd6\xcf\x8ako\x7f\xee\x1f\x0bCqoU\xe4\x13\xd4\xb8z`9"
+
+    secret1 = bytes([1] * 32)
+    secret2 = bytes([x * 314159 % 256 for x in range(32)])
+    sk1 = PrivateKey.from_bytes(secret1)
+    sk2 = PrivateKey.from_bytes(secret2)
+    
+    msg = bytes([3, 1, 4, 1, 5, 9])
+    sig1Basic = BScheme.sign(sk1, msg)
+    sig2Basic = BScheme.sign(sk2, msg)
+    sigABasic = BScheme.aggregate([sig1Basic, sig2Basic])
+    sig1Aug = AScheme.sign(sk1, msg)
+    sig2Aug = AScheme.sign(sk2, msg)
+    sigAAug = AScheme.aggregate([sig1Aug, sig2Aug])
+    sig1Pop = PScheme.sign(sk1, msg)
+    sig2Pop = PScheme.sign(sk2, msg)
+    sigAPop = PScheme.aggregate([sig1Pop, sig2Pop])
+
+    def compare(sig: bytes, ref_sig: bytes):
+        # In py_ecc, the 48th byte starts with 3 zeros
+        assert len(sig) == len(ref_sig)
+        for i, x in enumerate(sig):
+            if i != 48:
+                assert x == ref_sig[i]
+        assert int(sig[0]) & 0xe0 == int(sig[48]) & 0xe0
+    
+    compare(bytes(sig1Basic), ref_sig1Basic)
+    compare(bytes(sig2Basic), ref_sig2Basic)
+    compare(bytes(sigABasic), ref_sigABasic)
+    compare(bytes(sig1Aug), ref_sig1Aug)
+    compare(bytes(sig2Aug), ref_sig2Aug)
+    compare(bytes(sigAAug), ref_sigAAug)
+    compare(bytes(sig1Pop), ref_sig1Pop)
+    compare(bytes(sig2Pop), ref_sig2Pop)
+    compare(bytes(sigAPop), ref_sigAPop)
 
 test_schemes()
 test_elements()
 test_vectors_invalid()
 test_vectors_valid()
-# test2()
-# test_threshold()
-# test_vectors()
-# test_vectors2()
-# test_vectors3()
-# test_vectors4()
-# no_throw_bad_sig()
-# throw_wrong_type()
-# additional_python_methods()
-
 
 print("\nAll tests passed.")
 
 """
-Copyright 2018 Chia Network Inc
+Copyright 2020 Chia Network Inc
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
