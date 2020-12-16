94de66cd21c3db738e5190e631017658bfadfb06
xdustinface <xdustinfacex@gmail.com>
2020-12-16 03:47:21 +0100

    bench: Refactor Scheme:: => Scheme().

--- src/test-bench.cpp
+++ src/test-bench.cpp
@@ -31,13 +31,13 @@ using namespace bls;
 void benchSigs() {
     string testName = "Sigining";
     double numIters = 5000;
-    PrivateKey sk = AugSchemeMPL::KeyGen(getRandomSeed());
+    PrivateKey sk = AugSchemeMPL().KeyGen(getRandomSeed());
     vector<uint8_t> message1 = sk.GetG1Element().Serialize();
 
     auto start = startStopwatch();
 
     for (size_t i = 0; i < numIters; i++) {
-        AugSchemeMPL::Sign(sk, message1);
+        AugSchemeMPL().Sign(sk, message1);
     }
     endStopwatch(testName, start, numIters);
 }
@@ -45,7 +45,7 @@ void benchSigs() {
 void benchVerification() {
     string testName = "Verification";
     double numIters = 10000;
-    PrivateKey sk = AugSchemeMPL::KeyGen(getRandomSeed());
+    PrivateKey sk = AugSchemeMPL().KeyGen(getRandomSeed());
     G1Element pk = sk.GetG1Element();
 
     std::vector<G2Element> sigs;
@@ -54,7 +54,7 @@ void benchVerification() {
         uint8_t message[4];
         Util::IntToFourBytes(message, i);
         vector<uint8_t> messageBytes(message, message + 4);
-        sigs.push_back(AugSchemeMPL::Sign(sk, messageBytes));
+        sigs.push_back(AugSchemeMPL().Sign(sk, messageBytes));
     }
 
     auto start = startStopwatch();
@@ -62,7 +62,7 @@ void benchVerification() {
         uint8_t message[4];
         Util::IntToFourBytes(message, i);
         vector<uint8_t> messageBytes(message, message + 4);
-        bool ok = AugSchemeMPL::Verify(pk, messageBytes, sigs[i]);
+        bool ok = AugSchemeMPL().Verify(pk, messageBytes, sigs[i]);
         ASSERT(ok);
     }
     endStopwatch(testName, start, numIters);
@@ -79,19 +79,19 @@ void benchBatchVerification() {
         uint8_t message[4];
         Util::IntToFourBytes(message, i);
         vector<uint8_t> messageBytes(message, message + 4);
-        PrivateKey sk = AugSchemeMPL::KeyGen(getRandomSeed());
+        PrivateKey sk = AugSchemeMPL().KeyGen(getRandomSeed());
         G1Element pk = sk.GetG1Element();
-        sigs.push_back(AugSchemeMPL::Sign(sk, messageBytes));
+        sigs.push_back(AugSchemeMPL().Sign(sk, messageBytes));
         pks.push_back(pk);
         ms.push_back(messageBytes);
     }
 
     auto start = startStopwatch();
-    G2Element aggSig = AugSchemeMPL::Aggregate(sigs);
+    G2Element aggSig = AugSchemeMPL().Aggregate(sigs);
     endStopwatch("Aggregation", start, numIters);
 
     start = startStopwatch();
-    bool ok = AugSchemeMPL::AggregateVerify(pks, ms, aggSig);
+    bool ok = AugSchemeMPL().AggregateVerify(pks, ms, aggSig);
     ASSERT(ok);
     endStopwatch("Batch verification", start, numIters);
 }
@@ -105,27 +105,27 @@ void benchFastAggregateVerification() {
     vector<G2Element> pops;
 
     for (size_t i = 0; i < numIters; i++) {
-        PrivateKey sk = PopSchemeMPL::KeyGen(getRandomSeed());
+        PrivateKey sk = PopSchemeMPL().KeyGen(getRandomSeed());
         G1Element pk = sk.GetG1Element();
-        sigs.push_back(PopSchemeMPL::Sign(sk, message));
-        pops.push_back(PopSchemeMPL::PopProve(sk));
+        sigs.push_back(PopSchemeMPL().Sign(sk, message));
+        pops.push_back(PopSchemeMPL().PopProve(sk));
         pks.push_back(pk);
     }
 
     auto start = startStopwatch();
-    G2Element aggSig = PopSchemeMPL::Aggregate(sigs);
+    G2Element aggSig = PopSchemeMPL().Aggregate(sigs);
     endStopwatch("PopScheme Aggregation", start, numIters);
 
 
     start = startStopwatch();
     for (size_t i = 0; i < numIters; i++) {
-        bool ok = PopSchemeMPL::PopVerify(pks[i], pops[i]);
+        bool ok = PopSchemeMPL().PopVerify(pks[i], pops[i]);
         ASSERT(ok);
     }
     endStopwatch("PopScheme Proofs verification", start, numIters);
 
     start = startStopwatch();
-    bool ok = PopSchemeMPL::FastAggregateVerify(pks, message, aggSig);
+    bool ok = PopSchemeMPL().FastAggregateVerify(pks, message, aggSig);
     ASSERT(ok);
     endStopwatch("PopScheme verification", start, numIters);
 }
