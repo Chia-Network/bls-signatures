9f076748fd58a1de2f620ee01158aded4077f351
xdustinface <xdustinfacex@gmail.com>
2021-02-24 15:14:47 +0100

    schemes: Drop redundant default assignment

--- src/schemes.cpp
+++ src/schemes.cpp
@@ -116,7 +116,7 @@ bool CoreMPL::Verify(const G1Element& pubkey, const Bytes& message, const G2Elem
 
 vector<uint8_t> CoreMPL::Aggregate(const vector<vector<uint8_t>> &signatures)
 {
-    vector<G2Element> elements = vector<G2Element>();
+    vector<G2Element> elements;
     for (const vector<uint8_t>& signature : signatures) {
         elements.push_back(G2Element::FromByteVector(signature));
     }
