9f97b8b99e6c75cf34e02a313936358f096389a7
William Blanke <wjb98672@gmail.com>
2023-06-16 18:33:57 -0700

    import setuptools

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import platform
 import subprocess
 import sys
-from setuptools import Extension, setup
+from setuptools import Extension, setup, setuptools
 from setuptools.command.build_ext import build_ext
 
 
