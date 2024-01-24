d9e46025bae11c7a376c9172a28252ae70268c8b
Ronald Goedeke <ronald.goedeke@outlook.com>
2024-01-24 03:48:30 +0100

    Update blsjs.d.ts
    Updated blsjs.d.ts type inside the G1Element generator to return G1Element and not G2Element, or is this correct ?

--- js-bindings/blsjs.d.ts
+++ js-bindings/blsjs.d.ts
@@ -41,7 +41,7 @@ export declare class PopSchemeMPL {
 export declare class G1Element {
   static SIZE: number;
   static from_bytes(bytes: Uint8Array): G1Element;
-  static generator(): G2Element;
+  static generator(): G1Element;
   serialize(): Uint8Array;
   negate(): G1Element;
   deepcopy(): G1Element;
